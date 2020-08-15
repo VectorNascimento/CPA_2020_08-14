03af726914fc3b7ed42f223296aadfd742469b46
VectorNascimento <68569559+VectorNascimento@users.noreply.github.com>
2020-08-14 21:35:11 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,8 @@
+var frames = 1;
+var intervalo = setInterval(function(){
+	frames++;
+	document.querySelector(".frame").src="IMG/"+frames+".png";
+	if (frames == 26) {
+		clearInterval(intervalo);
+	}
+}, 50);
